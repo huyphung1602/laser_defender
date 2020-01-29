ba76920fcc776a628571c0af225d0381c8a2bf22
huyphung1602 <huy.phung.sw@gmail.com>
2020-01-29 15:08:52 +0700

    Added all the files from local

---
+++ Assets/Scripts/DamDealer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamDealer : MonoBehaviour
+{
+    [SerializeField] int damage = 100;
+    [SerializeField] float durationOfHit = 0.2f;
+
+    public int GetDamage() {
+        return damage;
+    }
+
+    public void Hit()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] float health = 100;
+    [SerializeField] float shotCounter;
+    [SerializeField] float minTimeBetweenShots = 0.2f;
+    [SerializeField] float maxTimeBetweenShots = 3f;
+    [SerializeField] GameObject laserPrefab;
+    [SerializeField] float protileSpeed = 10f;
+    [SerializeField] GameObject deathVFX;
+    [SerializeField] float durationOfExplosion = 1f;
+    [SerializeField] GameObject hitVFX;
+    [SerializeField] float durationOfHit = 1f;
+    [SerializeField] AudioClip deathSound;
+    [SerializeField] [Range(0, 1)] float deathSoundVolume = 0.75f;
+    [SerializeField] AudioClip hitSound;
+    [SerializeField] [Range(0, 1)] float hitSoundVolume = 0.75f;
+    [SerializeField] AudioClip shotSound;
+    [SerializeField] [Range(0, 1)] float shotSoundVolume = 0.25f;
+
+    void Start()
+    {
+        shotCounter = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+    }
+
+    void Update()
+    {
+        CountDownAndShoot();
+    }
+
+    private void CountDownAndShoot()
+    {
+        shotCounter -= Time.deltaTime;
+        if (shotCounter <= 0)
+        {
+            Fire();
+            shotCounter = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+        }
+    }
+
+    private void Fire()
+    {
+        GameObject laser = Instantiate(
+            laserPrefab,
+            transform.position,
+            Quaternion.identity)
+            as GameObject;
+
+        laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -protileSpeed);
+        AudioSource.PlayClipAtPoint(shotSound, Camera.main.transform.position, shotSoundVolume);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        DamDealer damageDealer = other.gameObject.GetComponent<DamDealer>();
+        if (!damageDealer) { return; }
+        ProcessHit(damageDealer);
+    }
+
+    private void ProcessHit(DamDealer damageDealer)
+    {
+        health -= damageDealer.GetDamage();
+        damageDealer.Hit();
+
+        if (health <= 0)
+        {
+            Die();
+        } else
+        {
+            GameObject hit = Instantiate(hitVFX, transform.position, transform.rotation);
+            Destroy(hit, durationOfHit);
+            AudioSource.PlayClipAtPoint(hitSound, Camera.main.transform.position, hitSoundVolume);
+        }
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+        GameObject explosion = Instantiate(deathVFX, transform.position, transform.rotation);
+        Destroy(explosion, durationOfExplosion);
+        AudioSource.PlayClipAtPoint(deathSound, Camera.main.transform.position, deathSoundVolume);
+    }
+}
